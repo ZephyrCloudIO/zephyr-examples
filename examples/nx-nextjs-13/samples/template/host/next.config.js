b660fe4c8179c173719ecba2c57c9a06a5c9da82
Bruno Silva <bruno3dcontato@gmail.com>
2023-09-27 13:39:30 -0300

    feat: add dashboard-plugin base config

--- examples/nx-nextjs-13/samples/template/host/next.config.js
+++ examples/nx-nextjs-13/samples/template/host/next.config.js
@@ -1,5 +1,6 @@
 const { composePlugins, withNx } = require('@nx/next');
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const DashboardPlugin = require('@module-federation/dashboard-plugin');
 
 const REMOTE_APP_URL =
   process.env.NEXT_PUBLIC_REMOTE_APP_URL || 'http://localhost:3001';
@@ -27,17 +28,37 @@ const nextConfig = {
   },
   webpack: (config, options) => {
     const { isServer } = options;
+
+    const mfPluginOptions = {
+      name: 'host',
+      filename: 'static/chunks/remoteEntry.js',
+      remotes: remotes(isServer),
+      exposes: {
+        // whatever else
+      },
+      shared: {
+        // whatever else
+        shared: require('./package.json').dependencies,
+      },
+    };
+
+    config.plugins.push(new NextFederationPlugin(mfPluginOptions));
+
     config.plugins.push(
-      new NextFederationPlugin({
-        name: 'host',
-        filename: 'static/chunks/remoteEntry.js',
-        remotes: remotes(isServer),
-        exposes: {
-          // whatever else
+      new DashboardPlugin({
+        versionStrategy: `${Date.now()}`,
+        filename: 'dashboard.json',
+        dashboardURL: `https://api-dev.zephyr-cloud.io/update?token=${process.env.DASHBOARD_WRITE_TOKEN}`,
+        versionChangeWebhook: 'http://cnn.com/',
+        metadata: {
+          clientUrl: process.env.DASHBOARD_BASE_URL,
+          baseUrl: 'http://localhost:3000',
+          remote: 'http://localhost:3000/remoteEntry.js',
+          source: {
+            url: 'https://github.com/ZephyrCloudIO/zephyr-examples',
+          },
         },
-        shared: {
-          // whatever else
-        },
+        standalone: mfPluginOptions,
       })
     );
 
