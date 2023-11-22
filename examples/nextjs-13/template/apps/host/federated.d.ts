3c3770f3424e834fa0b8c14b1226cc027ec13441
Bruno Silva <bruno3dcontato@gmail.com>
2023-11-22 00:42:13 -0300

    chore: remove old typedefs

--- examples/nextjs-13/template/apps/host/federated.d.ts
+++ examples/nextjs-13/template/apps/host/federated.d.ts
@@ -3,9 +3,3 @@ declare module 'remote/table' {
   import table from '@remote/components/table/table';
   export default table;
 }
-
-declare module 'remoteRaw/table' {
-  export * from '@remote/components/table/table';
-  import table from '@remote/components/table/table';
-  export default table;
-}
