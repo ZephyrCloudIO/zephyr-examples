71b11fe73847dbdc4088030a57df3b4971355148
Yokota <ryok90@gmail.com>
2023-11-30 11:36:53 -0300

    fix: move from app folder to pages

---
+++ examples/nextjs-13/nextjs-13-airbnb-clone/apps/host/actions/getListingById.ts
@@ -0,0 +1,30 @@
+import { listingsMock, usersMock } from '../mocks';
+
+interface IParams {
+  listingId?: string;
+}
+
+export default function getListingById(params: IParams) {
+  try {
+    const { listingId } = params;
+
+    const found = listingsMock.find(({ id }) => id === listingId);
+
+    if (!found) return null;
+
+    const listing = { ...found, user: usersMock[0] };
+
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: listing.user.emailVerified?.toString() || null,
+      },
+    };
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
---
+++ examples/nextjs-13/nextjs-13-airbnb-clone/apps/host/pages/layout.tsx
@@ -0,0 +1,42 @@
+import { Nunito } from 'next/font/google';
+import Navbar from '@/components/navbar/Navbar';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
+import SearchModal from '@/components/modals/SearchModal';
+import RentModal from '@/components/modals/RentModal';
+
+import ToasterProvider from '@/providers/ToasterProvider';
+
+import ClientOnly from '../components/ClientOnly';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+};
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { currentUser } = useCurrentUser();
+
+  return (
+    <div className={font.className}>
+      <ClientOnly>
+        <ToasterProvider />
+        <LoginModal />
+        <RegisterModal />
+        <SearchModal />
+        <RentModal />
+        <Navbar currentUser={currentUser} />
+      </ClientOnly>
+      <div className="pb-20 pt-28">{children}</div>
+    </div>
+  );
+}
