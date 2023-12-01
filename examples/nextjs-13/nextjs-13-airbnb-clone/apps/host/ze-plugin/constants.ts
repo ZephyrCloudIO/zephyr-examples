a5aec8a6274ac1250ae3852d7a1b0d53e00b04fa
Yokota <ryok90@gmail.com>
2023-12-01 08:17:07 -0300

    chore: add zephyr plugin to host

---
+++ examples/nextjs-13/nextjs-13-airbnb-clone/apps/host/ze-plugin/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_API_URL = 'https://api.zephyr-cloud.io/';
+
+export const DEFAULT_VERSIONED_REMOTE_NAME_SCHEMA = '__REMOTE_NAME__' + '_' + '__REMOTE_VERSION__';
+
+export const DEBUG_LOG_HEADER = 'ZE-LOG';
+export const DEBUG_ERROR_HEADER = 'ZE-ERROR';
+export const DEBUG_WARNING_HEADER = 'ZE-WARN';
