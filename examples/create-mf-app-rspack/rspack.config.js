01fc5b19bdea34700066e01b7fb6f51f211f7bde
Zack Chapple <zackarychapple30@gmail.com>
2024-04-04 17:04:48 -0400

    feat: adding create-mf-app

---
+++ examples/create-mf-app-rspack/rspack.config.js
@@ -0,0 +1,97 @@
+const rspack = require('@rspack/core')
+const refreshPlugin = require('@rspack/plugin-react-refresh')
+const {withZephyr} = require('zephyr-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development'
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = withZephyr()({
+  context: __dirname,
+  entry: {
+    main: './src/index.ts',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.js','.jsx','.ts','.tsx','.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: {
+                  tailwindcss: {},
+                  autoprefixer: {},
+                },
+              },
+            },
+          },
+        ],
+        type: 'css',
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: {
+                targets: [
+                  'chrome >= 87',
+                  'edge >= 88',
+                  'firefox >= 78',
+                  'safari >= 14',
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.container.ModuleFederationPlugin({
+      name: 'create_mf_app_rspack',
+      filename: 'remoteEntry.js',
+      exposes: {},
+      shared: {
+        react: { eager: true },
+        'react-dom': { eager: true },
+        'react-router-dom': { eager: true },
+      },
+    }),
+    new rspack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new rspack.ProgressPlugin({}),
+    new rspack.HtmlRspackPlugin({
+      template: './src/index.html',
+    }),
+    isDev ? new refreshPlugin() : null,
+  ].filter(Boolean),
+})
