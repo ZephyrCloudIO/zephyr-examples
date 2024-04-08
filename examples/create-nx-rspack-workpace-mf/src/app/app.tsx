d4fd1ef875eef4ebdc5c72998ad85b6add3067c6
Zack Chapple <zackarychapple30@gmail.com>
2024-04-08 08:29:12 -0400

    feat: initial nx-rspack commit

---
+++ examples/create-nx-rspack-workpace-mf/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="create-nx-rspack-workpace-mf" />
+    </div>
+  );
+}
+
+export default App;
