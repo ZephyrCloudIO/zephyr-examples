27dbc11109c03aeacfb03c5a1265391e3ae3c865
Rodrigo Yokota <ryok90@gmail.com>
2024-04-12 09:51:30 -0300

    fix: remove changes of deleted files

--- examples/nextjs-13/template/apps/host/remote-map.js
+++
@@ -1,7 +0,0 @@
-// Here we can define the remote entry points for our remotes
-const remoteMap = {
-  remote: `http://localhost:3011/_next/static/__LOCATION__/remoteEntry.js`,
-  // whatever else
-};
-
-module.exports = remoteMap;
