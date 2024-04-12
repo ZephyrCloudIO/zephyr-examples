0095801f7993367aa463b70454c71f3289f27e69
RomSribn <steeper.fest@gmail.com>
2024-04-12 16:13:51 +0200

    feat: create-default-webpack-mf valid withZephyr deploy

---
+++ examples/create-default-webpack-mf/app1/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { withZephyr } = require("zephyr-webpack-plugin");
+const {ModuleFederationPlugin} = require("webpack").container;
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
+
+module.exports = withZephyr()({
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3001,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app1",
+      remotes: {
+        app2: "app2@[app2Url]/remoteEntry.js",
+      },
+      shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+    }),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+});
+
---
+++ examples/create-default-webpack-mf/app2/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { withZephyr } = require("zephyr-webpack-plugin");
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = withZephyr()({
+  entry: './src/index',
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3002,
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    // To learn more about the usage of this plugin, please visit https://webpack.js.org/plugins/module-federation-plugin/
+    new ModuleFederationPlugin({
+      name: 'app2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+});
