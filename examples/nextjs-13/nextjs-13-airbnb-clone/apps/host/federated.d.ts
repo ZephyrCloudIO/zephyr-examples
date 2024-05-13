a2970fac08b2caeb118c649e614806f55cce8992
Rodrigo Yokota <ryok90@gmail.com>
2024-05-13 16:47:05 -0300

    chore: remove next airbnb example

--- examples/nextjs-13/nextjs-13-airbnb-clone/apps/host/federated.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'airbnb-remote1/ListingCategory' {
-  export * from '@airbnb-remote1/components/ListingCategory';
-  import ListingCategory from '@airbnb-remote1/components/ListingCategory';
-  export default ListingCategory;
-}
--- examples/nextjs-13/nextjs-13-airbnb-clone/apps/remote1/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <h1 className="text-center pt-2">This is the remote</h1>;
-};
-
-export default Home;
