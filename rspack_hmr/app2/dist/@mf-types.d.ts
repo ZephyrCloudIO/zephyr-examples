e162e9d5c72ec5610a5891e635ffebb07d7cb9e2
zmzlois <lois.z@comcord.vision>
2024-07-06 03:09:42 +0100

    remove rspack-hmr

--- rspack_hmr/app2/dist/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/Hello' | 'REMOTE_ALIAS_IDENTIFIER/pi';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/pi' ? typeof import('REMOTE_ALIAS_IDENTIFIER/pi') :T extends 'REMOTE_ALIAS_IDENTIFIER/Hello' ? typeof import('REMOTE_ALIAS_IDENTIFIER/Hello') :any;
