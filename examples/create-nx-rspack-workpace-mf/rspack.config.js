2c1a409552cb3aa5c81bc9f00ed632ae2d351600
SvetlanaMuravlova <svetlana.muravlova@valor-software.com>
2024-09-06 14:49:04 +0300

    feat(chore): updated plugin version

--- examples/create-nx-rspack-workpace-mf/rspack.config.js
+++ examples/create-nx-rspack-workpace-mf/rspack.config.js
@@ -2,5 +2,5 @@ const { composePlugins, withNx, withReact } = require('@nx/rspack');
 const {withZephyr} = require('zephyr-webpack-plugin');
 
 module.exports = composePlugins(withNx(), withReact(), withZephyr(), (config) => {
-  return config;
+    return config;
 });
