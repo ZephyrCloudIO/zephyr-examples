7c33f7fe39e6d7194b0db442d4c9cbed8cdc5dd5
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2024-09-20 08:50:38 +0300

    feat(chore): updated lib versions in ng-nx-v15 example (#34)
    * feat(chore): updated nx-v15-ng-v14 libs versions
    
    * feat(chore): updated plugin version
    
    * fix(chore): plugin v.0.0.17
    
    * fix(chore): fixed package-lock file

--- examples/ng-nx-v15/src/main.ts
+++ examples/ng-nx-v15/src/main.ts
@@ -1,11 +1,8 @@
+import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { appRoutes } from './app/app.routes';
+
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
