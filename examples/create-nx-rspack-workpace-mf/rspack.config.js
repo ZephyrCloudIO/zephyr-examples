3b032194fc43c108b46a2cac6d7be85a2e3bbd03
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2024-09-20 08:45:26 +0300

    feat(chore): updated lib versions in create-nx-rspack-workpace-mf example (#33)
    * feat(chore): updated libs versions
    
    * feat(chore): updated plugin version
    
    * update nx rspack example
    
    * update
    
    * fix(chore): added build script
    
    ---------
    
    Co-authored-by: zmzlois <lois.z@comcord.vision>

--- examples/create-nx-rspack-workpace-mf/rspack.config.js
+++
@@ -1,6 +0,0 @@
-const { composePlugins, withNx, withReact } = require('@nx/rspack');
-const {withZephyr} = require('zephyr-webpack-plugin');
-
-module.exports = composePlugins(withNx(), withReact(), withZephyr(), (config) => {
-  return config;
-});
