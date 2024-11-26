d550001836b0fc409b5c5369abe3033c7777c9f3
Shane <shane@swalker.dev>
2024-11-26 10:13:26 -0600

    added history fallback to host config

--- examples/turbo-rspack-mf/apps/host/rspack.config.ts
+++ examples/turbo-rspack-mf/apps/host/rspack.config.ts
@@ -11,24 +11,25 @@ const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
 export default withZephyr()({
   context: __dirname,
   devServer: {
-    port: 3000
+    historyApiFallback: true,
+    port: 3000,
   },
   entry: {
-    main: "./src/main.tsx"
+    main: "./src/main.tsx",
   },
   resolve: {
-    extensions: ["...", ".ts", ".tsx", ".jsx"]
+    extensions: ["...", ".ts", ".tsx", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["postcss-loader"],
-        type: "css"
+        type: "css",
       },
       {
         test: /\.svg$/,
-        type: "asset"
+        type: "asset",
       },
       {
         test: /\.(jsx?|tsx?)$/,
@@ -39,39 +40,39 @@ export default withZephyr()({
               jsc: {
                 parser: {
                   syntax: "typescript",
-                  tsx: true
+                  tsx: true,
                 },
                 transform: {
                   react: {
                     runtime: "automatic",
                     development: isDev,
-                    refresh: isDev
-                  }
-                }
+                    refresh: isDev,
+                  },
+                },
               },
-              env: { targets }
-            }
-          }
-        ]
-      }
-    ]
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new rspack.HtmlRspackPlugin({
-      template: "./index.html"
+      template: "./index.html",
     }),
     isDev ? new RefreshPlugin() : null,
-    new rspack.container.ModuleFederationPlugin(mfConfig)
+    new rspack.container.ModuleFederationPlugin(mfConfig),
   ].filter(Boolean),
   optimization: {
     minimizer: [
       new rspack.SwcJsMinimizerRspackPlugin(),
       new rspack.LightningCssMinimizerRspackPlugin({
-        minimizerOptions: { targets }
-      })
-    ]
+        minimizerOptions: { targets },
+      }),
+    ],
   },
   experiments: {
-    css: true
-  }
+    css: true,
+  },
 });
