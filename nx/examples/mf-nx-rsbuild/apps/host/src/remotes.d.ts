7cddcd31cda2b4d8a0e05690c727a1d467438e19
Néstor <nestor@nstlopez.com>
2025-11-04 12:20:51 +0100

    chore: new example with rspack

--- nx/examples/mf-nx-rsbuild/apps/host/src/remotes.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'remote1/Module' {
-  const Module: React.ComponentType;
-  export default Module;
-}
-
-declare module 'remote2/Module' {
-  const Module: React.ComponentType;
-  export default Module;
-}
---
+++ nx/examples/mf-nx-rspack/apps/remote1/src/app/app.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div>
+      <h1>Remote 1</h1>
+      <p>This is the Remote 1 application loaded via Module Federation</p>
+    </div>
+  );
+}
+
+export default App;
