0725fddea87ac314ce05b2e16499d0cc8d806fb9
Rodrigo Yokota <ryok90@gmail.com>
2026-03-17 15:48:07 -0300

    fix: undo strict

--- turborepo/examples/mf-turbo-rspack/apps/home/module-federation.config.ts
+++ turborepo/examples/mf-turbo-rspack/apps/home/module-federation.config.ts
@@ -13,20 +13,17 @@ export const mfConfig: ModuleFederationPluginOptions = {
     react: {
       singleton: true,
       eager: true,
-      requiredVersion: "19.2.0",
-      strictVersion: true
+      requiredVersion: "^19.2.0"
     },
     "react-dom": {
       singleton: true,
       eager: true,
-      requiredVersion: "19.2.0",
-      strictVersion: true
+      requiredVersion: "^19.2.0"
     },
     "react-router": {
       singleton: true,
       eager: true,
-      requiredVersion: "7.9.5",
-      strictVersion: true
+      requiredVersion: "^7.9.5"
     }
   }
 };
