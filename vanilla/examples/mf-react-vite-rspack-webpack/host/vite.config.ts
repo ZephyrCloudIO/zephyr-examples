86e58495fe32ad1e76c82b40b3f003400bda6476
Néstor <nestor@nstlopez.com>
2026-03-27 13:54:01 +0100

    fix: avoid vite plugin zephyr type re-export (#141)

--- vanilla/examples/mf-react-vite-rspack-webpack/host/vite.config.ts
+++ vanilla/examples/mf-react-vite-rspack-webpack/host/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { withZephyr, ModuleFederationOptions } from 'vite-plugin-zephyr';
+import { withZephyr } from 'vite-plugin-zephyr';
 
-const mfConfig: ModuleFederationOptions = {
+const mfConfig = {
   name: 'vite-host',
   filename: 'remoteEntry.js',
   remotes: {
